020d0a788bf19e3ec1731244da00b34287e01746
Troels Henriksen <athas@sigkill.dk>
2018-08-19 17:55:10 +0200

    More precise types and such.

--- game.go
+++ game.go
@@ -10,6 +10,7 @@ import "C"
 
 import (
 	"unsafe"
+	"os"
 )
 
 type Game struct {
@@ -17,8 +18,8 @@ type Game struct {
 	ctx       *C.struct_futhark_context
 	state     *C.struct_futhark_opaque_ext_game_state
 	Frame     unsafe.Pointer
-	screenX   C.int
-	screenY   C.int
+	screenX   C.int32_t
+	screenY   C.int32_t
 	nameCache map[C.uchar]string
 }
 
@@ -30,17 +31,17 @@ type Element struct {
 func NewGame(screenX, screenY int) Game {
 	// Setup Futhark-side stuff.
 	cfg := C.futhark_context_config_new()
-	C.futhark_context_config_set_device(cfg, C.CString(""))
+	C.futhark_context_config_set_device(cfg, C.CString(os.Getenv("OPENCL_DEVICE")))
 
 	ctx := C.futhark_context_new(cfg)
 
 	var state *C.struct_futhark_opaque_ext_game_state
-	C.futhark_entry_new_game(ctx, &state, C.int(screenX), C.int(screenY))
+	C.futhark_entry_new_game(ctx, &state, C.int32_t(screenX), C.int32_t(screenY))
 
 	frame := C.malloc(C.ulong(screenX * screenY * 4))
 
 	return Game{
-		cfg, ctx, state, frame, C.int(screenX), C.int(screenY),
+		cfg, ctx, state, frame, C.int32_t(screenX), C.int32_t(screenY),
 		make(map[C.uchar]string),
 	}
 }
@@ -61,9 +62,9 @@ func (g Game) Render(ul_x, ul_y, scale float64, screenX, screenY int) {
 	var frame_fut *C.struct_futhark_i32_2d
 	C.futhark_entry_render(g.ctx, &frame_fut, g.state,
 		C.float(ul_x), C.float(ul_y), C.float(scale),
-		C.int(screenX), C.int(screenY))
+		C.int32_t(screenX), C.int32_t(screenY))
 	defer C.futhark_free_i32_2d(g.ctx, frame_fut)
-	C.futhark_values_i32_2d(g.ctx, frame_fut, (*C.int)(g.Frame))
+	C.futhark_values_i32_2d(g.ctx, frame_fut, (*C.int32_t)(g.Frame))
 }
 
 func (g *Game) AddElem(
@@ -73,7 +74,7 @@ func (g *Game) AddElem(
 	defer C.futhark_free_opaque_ext_game_state(g.ctx, g.state)
 	C.futhark_entry_add_element(
 		g.ctx, &g.state, g.state,
-		C.float(ul_x), C.float(ul_y), C.float(scale), C.int(g.screenX), C.int(g.screenY),
+		C.float(ul_x), C.float(ul_y), C.float(scale), C.int32_t(g.screenX), C.int32_t(g.screenY),
 		C.int32_t(from_x), C.int32_t(from_y), C.int32_t(to_x), C.int32_t(to_y),
 		C.int32_t(radius), C.uint8_t(what.code))
 }
@@ -85,7 +86,7 @@ func (g *Game) ClearElem(
 	defer C.futhark_free_opaque_ext_game_state(g.ctx, g.state)
 	C.futhark_entry_clear_element(
 		g.ctx, &g.state, g.state,
-		C.float(ul_x), C.float(ul_y), C.float(scale), C.int(g.screenX), C.int(g.screenY),
+		C.float(ul_x), C.float(ul_y), C.float(scale), C.int32_t(g.screenX), C.int32_t(g.screenY),
 		C.int32_t(from_x), C.int32_t(from_y), C.int32_t(to_x), C.int32_t(to_y),
 		C.int32_t(radius))
 }
@@ -129,7 +130,7 @@ func (g Game) ElementAt(ul_x, ul_y, scale float64, x, y int32) Element {
 	C.futhark_entry_element_at(
 		g.ctx, &element, g.state,
 		C.float(ul_x), C.float(ul_y), C.float(scale), g.screenX, g.screenY,
-		C.int(x), C.int(y))
+		C.int32_t(x), C.int32_t(y))
 	return Element{element, g.elementName(element)}
 }
 
